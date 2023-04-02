8afd4a8e9dc285619482e9d8262da2e112cec717
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-04-02 17:13:30 +0200

    FloatingInput Updated

--- src/Component/Auth/FloatingInput.js
+++ src/Component/Auth/FloatingInput.js
@@ -1,7 +1,7 @@
-export default function FloatingInput({ type, icon, label, className }) {
+export default function FloatingInput({ type, name, value, icon, label, className }) {
   return (
     <div className={className + " form-floating"}>
-      <input className="form-control shadow-none fw-bold" type={type} placeholder={label} />
+      <input className="form-control shadow-none fw-bold" type={type} name={name} value={value} placeholder={label} />
       <label className="form-label">
         <i className={icon + " ms-1 me-2"}></i>
         {label}
