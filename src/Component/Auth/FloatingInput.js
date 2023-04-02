e7b473d4aaf9e0de13927531f7369e6d9124d41c
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-04-02 19:29:34 +0200

    Buyer Profile Created

--- src/Component/Auth/FloatingInput.js
+++ src/Component/Auth/FloatingInput.js
@@ -1,7 +1,15 @@
-export default function FloatingInput({ type, name, value, icon, label, className }) {
+export default function FloatingInput({ type, name, defaultValue, onChange, icon, label, readonly, className }) {
   return (
     <div className={className + " form-floating"}>
-      <input className="form-control shadow-none fw-bold" type={type} name={name} value={value} placeholder={label} />
+      <input
+        className="form-control shadow-none fw-bold"
+        type={type}
+        name={name}
+        defaultValue={defaultValue}
+        placeholder={label}
+        onChange={onChange}
+        readOnly = {readonly? true : false}
+        />
       <label className="form-label">
         <i className={icon + " ms-1 me-2"}></i>
         {label}
