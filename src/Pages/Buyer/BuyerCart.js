16c2060c6892a4711cece5d7e4007ec1760e9ad1
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-04-02 15:27:56 +0200

    Buyer Module Created

---
+++ src/Pages/Buyer/BuyerCart.js
@@ -0,0 +1,12 @@
+import BuyerTaps from "../../Component/Buyer/BuyerTaps"
+
+export default function BuyerCart() {
+  return (
+    <div>
+      <BuyerTaps active='Cart' />
+      <div className="container">
+        Cart
+      </div>
+    </div>
+  )
+}
---
+++ src/Pages/Buyer/BuyerFavList.js
@@ -0,0 +1,12 @@
+import BuyerTaps from "../../Component/Buyer/BuyerTaps"
+
+export default function BuyerFavList() {
+  return (
+    <div>
+      <BuyerTaps active='Favorite List' />
+      <div className="container">
+        Favorite List
+      </div>
+    </div>
+  )
+}
