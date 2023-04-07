871711ed70744ca038d7d4ee3edb53c8f835493f
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-04-07 13:39:45 +0200

    All Buyers Page Updated

---
+++ src/Component/Uitility/UserDataCard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function BuyerInfo({ name, email, phone, address }) {
+  return (
+    <div className='bg-white border-bottom position-relative overflow-hidden'>
+
+      <div className='bg-primary py-5 position-absolute top-0 start-0 w-100' style={{ zIndex: '1' }}></div>
+      <div className='text-center mt-5'>
+        <i className='fa-solid fa-user text-secondary p-4 fs-2 rounded-circle position-relative' style={{ background: '#eee', zIndex: '2' }}></i>
+      </div>
+
+      <div className='p-3 text-nowrap'>
+        <h4 className='fw-bold text-center'>{name}</h4>
+        <small className='text-muted'>
+          <p><i class="fa-solid fa-envelope"></i> {email}</p>
+          <p><i class="fa-solid fa-phone"></i> {phone}</p>
+          <p><i class="fa-solid fa-location-dot"></i> {address}</p>
+        </small>
+
+        <div className='d-flex mt-3'>
+          <Link to={'/admin/editUser/:id'} className='btn btn-primary btn-sm rounded-0 flex-fill'><i className='fa-solid fa-edit'></i> Edit</Link>
+          <Link className='btn btn-danger btn-sm rounded-0 flex-fill'><i className='fa-solid fa-trash'></i> Delete</Link>
+        </div>
+      </div>
+
+    </div>
+  )
+}
--- src/Pages/Admin/AdminAllBuyers.js
+++ src/Pages/Admin/AdminAllBuyers.js
@@ -1,49 +1,47 @@
 import AdminTabs from "../../Component/Admin/AdminTabs";
-import BuyerInfo from "../../Component/Buyer/BuyerInfo";
+import UserDataCard from "../../Component/Uitility/UserDataCard";
 
 export default function AdminAllBuyers() {
 
   const Buyers = [
     {
-      name : "Karimzz" , 
-      email : "Karimzz@gmail.com" ,
-      password : "12345" , 
-      address : "Cairo"
-    } ,
+      name: "Karimzz",
+      email: "Karimzz@gmail.com",
+      phone: "12345",
+      address: "Cairo"
+    },
     {
-      name : "Ahmed" , 
-      email : "Ahmed@gmail.com" ,
-      password : '243434' ,
-      address : "Zagaizg"
-    } ,
+      name: "Ahmed",
+      email: "Ahmed@gmail.com",
+      phone: '243434',
+      address: "Zagaizg"
+    },
     {
-      name  :"Mshref" , 
-      email : "Mshref@gmail.com", 
-      password : "232133"  ,
-      address : "London"
-    } ,
+      name: "Mshref",
+      email: "Mshref@gmail.com",
+      phone: "232133",
+      address: "London"
+    },
     {
-      name : "Ziad" , 
-      email : "Ziad@gmail.com" ,
-      password : "43432432" ,
-      address : "Alex"
-    } , 
+      name: "Ziad",
+      email: "Ziad@gmail.com",
+      phone: "43432432",
+      address: "Alex"
+    },
     {
-      name : "Moahmed" , 
-      email : "Moahmed@gmail.com"  ,
-      password : "23232" , 
-      address : "US"
+      name: "Mohamed",
+      email: "Mohamed@gmail.com",
+      phone: "23232",
+      address: "US"
     }
   ]
   return (
     <div>
       <AdminTabs active='All Buyers' />
-      <div className="container kcontainer">
-          {
-            Buyers.map((item)=>{
-              return <BuyerInfo name={item.name} email={item.email} password={item.password} address={item.address} />
-            })
-          }
+      <div className="container kazem-grid py-4">
+        {Buyers.map((item) =>
+          <UserDataCard name={item.name} email={item.email} phone={item.phone} address={item.address} />
+        )}
       </div>
     </div>
   )
