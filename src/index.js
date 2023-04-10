d9d60e792b47e0feada58d309ecca38f39bdb65b
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-04-10 15:07:46 +0200

    Store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { store } from './Testing Data/store';
+import { store } from './Testing Data/Store';
 import { Provider } from 'react-redux';
 
 import './Styles/master.css';
