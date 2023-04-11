b46f65e8ee1673513b0e6b126242b35f6d062181
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-04-12 01:29:04 +0200

    Authentication Linked

--- src/Pages/Auth/RegisterPage.js
+++ src/Pages/Auth/RegisterPage.js
@@ -1,22 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 import FloatingInput from '../../Component/Uitility/FloatingInput';
 
 export default function RegisterPage() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const register = async (event) => { 
+    event.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/auth/register', {
+        firstName,
+        lastName,
+        email,
+        phone,
+        address,
+        password
+      });
+      console.log('Success');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='d-flex align-items-center' style={{ minHeight: '85vh' }}>
       <div className="container my-5 py-5 px-4 px-sm-5 bg-white border-bottom border-top" style={{ maxWidth: "750px" }}>
         <h2 className="fw-bold text-center mb-3">Register</h2>
-        <form>
+
+        <form onSubmit={register}>
           <div className='d-flex gap-2 mb-2'>
-            <FloatingInput type='text' name='fname' required icon='fa-regular fa-user' label='First Name' className='flex-fill' />
-            <FloatingInput type='text' name='lname' required icon='fa-regular fa-user' label='Last Name' className='flex-fill' />
+            <FloatingInput type='text' name='fname' required onChange={(e) => setFirstName(e.target.value)} icon='fa-regular fa-user' label='First Name' className='flex-fill' />
+            <FloatingInput type='text' name='lname' required onChange={(e) => setLastName(e.target.value)} icon='fa-regular fa-user' label='Last Name' className='flex-fill' />
           </div>
           <div className='d-flex flex-column gap-2'>
-            <FloatingInput type='email' name='email' required icon='fa-solid fa-envelope' label='Email' />
-            <FloatingInput type='text' name='phone' required icon='fa-solid fa-phone' label='Phone Number' />
-            <FloatingInput type='text' name='address' required icon='fa-solid fa-location-dot' label='Address' />
-            <FloatingInput type='password' name='password' required icon='fa-solid fa-lock' label='Password' />
+            <FloatingInput type='email' name='email' required onChange={(e) => setEmail(e.target.value)} icon='fa-solid fa-envelope' label='Email' />
+            <FloatingInput type='number' name='phone' required onChange={(e) => setPhone(e.target.value)} icon='fa-solid fa-phone' label='Phone Number' />
+            <FloatingInput type='text' name='address'  required onChange={(e) => setAddress(e.target.value)} icon='fa-solid fa-location-dot' label='Address' />
+            <FloatingInput type='password' name='password' required onChange={(e) => setPassword(e.target.value)} icon='fa-solid fa-lock' label='Password' />
           </div>
 
           <div className="d-grid my-3">
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { store } from './Testing Data/store';
 import { Provider } from 'react-redux';
+import App from './App';
 
+import './Styles/fontawesome/all.min.css';
 import './Styles/master.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
