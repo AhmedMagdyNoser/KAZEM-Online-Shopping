c2fa6703cabde681f01333cf19d490efb66363eb
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-04-17 00:25:30 +0200

    categories pages updated

--- src/Pages/Categories/AllCategoriesPage.js
+++ src/Pages/Categories/AllCategoriesPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import SimpleCard from '../../Component/Uitility/SimpleCard'
 const api = require('../../api')
 
-export default function AllCategoryPage() {
+export default function AllCategoriesPage() {
 
   let [categories, setCategories] = useState([]);
 
@@ -17,9 +17,13 @@ export default function AllCategoryPage() {
   return (
     <div className='container pb-4'>
       <h2 className='my-4 py-3 bg-dark text-white text-center text-uppercase'>All Categories</h2>
-      <div className='kazem-sm-grid'>
-        {categories.map(category => <SimpleCard key={category.id} category={category} />)}
-      </div>
+      {
+        categories.length > 0 ? (
+          <div className='kazem-sm-grid'>
+            {categories.map(category => <SimpleCard key={category.id} category={category} />)}
+          </div>
+        ) : <h3 className="text-center my-5">No Categories</h3>
+      }
     </div>
   )
 }
--- src/Pages/Categories/CategoryDetailsPage.js
+++ src/Pages/Categories/CategoryDetailsPage.js
@@ -10,28 +10,34 @@ export default function CategoryDetailsPage() {
   let [products, setProducts] = useState([]);
 
   const params = useParams(); // it's the parameters in the current url '/category/:id' the only param is the id
-  
-  async function getCategory(id) {
-    await api.getCategory(id, setCategory);
+
+  async function getCategory() {
+    await api.getCategory(params.id, setCategory);
   }
 
-  async function getProductsOfCategory(id) {
-    await api.getProductsOfCategory(id, setProducts);
+  async function getProductsOfCategory() {
+    await api.getProductsOfCategory(params.id, setProducts);
   }
 
   useEffect(() => {
-    getCategory(params.id);
-    getProductsOfCategory(params.id);
+    getCategory();
+    getProductsOfCategory();
   }, [])
 
   return (
     <div className="container bg-white p-0 my-4 border-bottom">
       <h2 className='m-0 py-3 bg-dark text-white text-center text-uppercase'>{category.title}</h2>
-      <div className="kazem-grid p-3">
-        {products.map(product =>
-          <ProductCard key={product.id} product={product} />
-        )}
-      </div>
+      <p className='m-0 py-3  text-center'>{category.description}</p>
+      
+      {products.length > 0 ?
+        (
+          <div className="kazem-grid p-3">
+            {products.map(product =>
+              <ProductCard key={product.id} product={product} />
+            )}
+          </div>
+        )
+        : <h3 className="text-center my-5">No Products In This Category</h3>}
     </div>
   )
 }
